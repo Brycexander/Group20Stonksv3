d2ad606ca84d6bc51e997569296c156694501507
Brycexander <bryceahitchcock@gmail.com>
2021-03-31 18:14:38 -0400

    added links to frontend stuff

--- server.js
+++ server.js
@@ -32,5 +32,16 @@ require("./config/passport")(passport);
 
 app.use("/api/users", Users);
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
+
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('frontend/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
+
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
 

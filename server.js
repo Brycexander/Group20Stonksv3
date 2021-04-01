d6b2c41b15fe84c51a7a726d140099c2a54dcbe5
Brycexander <bryceahitchcock@gmail.com>
2021-04-01 13:01:26 -0400

    changed serverjs

--- server.js
+++ server.js
@@ -33,15 +33,19 @@ require("./config/passport")(passport);
 app.use("/api/Users", Users);
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 
-// Server static assets if in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('frontend/build'));
+// // Server static assets if in production
+// if (process.env.NODE_ENV === 'production') {
+//   // Set static folder
+//   app.use(express.static('frontend/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  });
-}
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+//   });
+// }
+
+app.get('/', function (req, res) {
+  res.render('index', {});
+});
 
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
 

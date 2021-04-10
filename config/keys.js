0cf7370031aa9966f3982378cd0fe23d6cf6ba62
Brycexander <bryceahitchcock@gmail.com>
2021-04-10 16:39:19 -0400

    please work

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 module.exports = {
     mongoURI: "mongodb+srv://ML13:Group20Rocks@stocksimulator.of8ep.mongodb.net/StockSimulatorApp?retryWrites=true&w=majority",
+    SENDGRID_API_KEY: "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",
     finnhubURI: "c1f0tcn48v6of5hb7f90",
     secretOrKey: "secret" 
   };

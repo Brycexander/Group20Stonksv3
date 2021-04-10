473cdec6d6cd1b6a5a984765f34a3d03d8d2dcc3
paopowpew <77464555+paopowpew@users.noreply.github.com>
2021-04-09 21:58:24 -0400

    Update bp.js

--- frontend/src/components/bp.js
+++ frontend/src/components/bp.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 exports.buildPath = function ( route )
 {
     const app_name = 'group20-stocksimulatorv2';
-    if (true) 
+    if (process.env.NODE_ENV === 'production') 
     {
         return 'https://' + app_name +  '.herokuapp.com/' + route;
     }

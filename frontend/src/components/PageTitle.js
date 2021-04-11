8c2aa8010b2e12551d0cf4c0c8f93fd9588a8ae7
paopowpew <paolabigio@knights.ucf.edu>
2021-04-11 08:56:16 -0400

    removed link from head

--- frontend/src/components/PageTitle.js
+++ frontend/src/components/PageTitle.js
@@ -16,7 +16,6 @@ function PageTitle()
   return(
     <>
     <head>
-      <link rel="stylesheet" href="../PageTitle.css"></link>
        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" 
       integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" 
       crossorigin="anonymous"></link>

bf7e5c662a0157a0552dd72171136e34415b9bb5
paopowpew <paolabigio@knights.ucf.edu>
2021-04-15 13:21:31 -0400

    pls display

--- frontend/src/components/Orders.js
+++ frontend/src/components/Orders.js
@@ -40,24 +40,7 @@ function createData(name, time, value, shares, avg) {
   return { name, time, value, shares, avg };
 }
 
-const rows = [
-  createData('Tesla', '11:90am', 123, 22, '3')
-  /*createData('Tesla', '11:45am', 124, 22),
-  createData('AMC', '1:29pm', 9.50, 30),
-  createData('Game Stop', '3:30am', 120, 7),
-  createData('Lululemon', '6:20pm', 200, 2),
-  createData('Apple', '2:14pm', 167, 10),
-  createData('Twitter', '10:29am', 63.90, 28),
-  createData('Microsoft', '9:50am', 242.55, 19),
-  createData('Disney', '9:00am', 188.90, 10),
-  createData('GoPro', '8:48am', 12.50, 8),
-  createData('Starbucks', '8:39am', 109.38, 100),
-  createData('Chegg', '10:00pm', 88.25, 12),
-  createData('Spotify', '12:00pm', 273.50, 2),
-  createData('Chase', '6:00pnm', 120.20, 8),
-  createData('Square', '8:05pm', 230.29, 1),
-  createData('DOW', '3:09pm', 64.60, 7),*/
-];
+const rows = [];
 
 function get(){
 
@@ -118,8 +101,9 @@ const useStyles = makeStyles({
 });
 
 export default function StickyHeadTable() {
-  get();
   
+  get();
+
   const classes = useStyles();
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);

8ad33095158abf202fb460f332a2600a21004a4a
Brycexander <bryceahitchcock@gmail.com>
2021-04-16 18:35:35 -0400

    changing buy

--- routes/api/Portfolios.js
+++ routes/api/Portfolios.js
@@ -19,26 +19,28 @@ router.post("/getPortfolio", (req, res) => {
 // API to Buy
 router.post("/buyStock", (req, res) => {
     Portfolio.findOne({ "Login": req.body.Login }).then(portfolio => {
-        if (req.body.Price * req.body.Amount > portfolio.Cash) {
-            return res.status(400).json("Insufficient founds");
-        } else {
-            var index = -1;
-            for (i = 0; i < portfolio.StocksOwned.length; i++){
-                if (portfolio.StocksOwned[i].Company == req.body.Company) {
-                    index = i;
-                    break;
-                }
-            }
-            if (index != -1) {
-                portfolio.StocksOwned[index].Amount += req.body.Amount;
-                portfolio.StocksOwned[index].TotalValue += req.body.Price * req.body.Amount;
+        Stock.findOne({ "Company": req.body.Company}).then(stock => {
+            if (stock.Quote.c * req.body.Amount > portfolio.Cash) {
+                return res.status(400).json("Insufficient founds");
             } else {
-                portfolio.StocksOwned.push({ Company: req.body.Company, Amount: req.body.Amount, StockValue: req.body.Price, TotalValue: req.body.Price * req.body.Amount, Date: Date.now() });
+                var index = -1;
+                for (i = 0; i < portfolio.StocksOwned.length; i++) {
+                    if (portfolio.StocksOwned[i].Company == req.body.Company) {
+                        index = i;
+                        break;
+                    }
+                }
+                if (index != -1) {
+                    portfolio.StocksOwned[index].Amount += req.body.Amount;
+                    portfolio.StocksOwned[index].TotalValue += stock.Quote.c * req.body.Amount;
+                } else {
+                    portfolio.StocksOwned.push({ Company: req.body.Company, Amount: req.body.Amount, StockValue: stock.Quote.c, TotalValue: stock.Quote.c * req.body.Amount, Date: Date.now() });
+                }
+                portfolio.Cash -= stock.Quote.c * req.body.Amount;
+                portfolio.Holdings += stock.Quote.c * req.body.Amount;
+                portfolio.save().then(res.status(200).json("Shares Bought"));
             }
-            portfolio.Cash -= req.body.Price * req.body.Amount;
-            portfolio.Holdings += req.body.Price * req.body.Amount;
-            portfolio.save().then(res.status(200).json("Shares Bought"));
-        }
+        });
     });
 });
 
